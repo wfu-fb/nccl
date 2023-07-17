3af91169e67ec4471b68e0818c4ee322929cb54f
Cen Zhao <cenzhao@meta.com>
2023-07-17 15:12:37 -0700

    add UT for commThreaded.h
    Summary:
    ## motivation
    let's strive for high UT coverage for newly introduced modules (today we seems have none)
    
    ## this diff
    - adds 1st UT for comm_threaded class `threadedRanksClique`. this only works for `buck2`, need a way to enable the OSS build as well (which we can look at it later)
    - exposed more headers to be public
    
    Reviewed By: pavanbalaji
    
    Differential Revision: D47219296
    
    fbshipit-source-id: cd5715dda0ef43e83ed9ea6e83a2caad585f2618

---
+++ src/tests/CommThreadedTest.cu
@@ -0,0 +1,18 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include <iostream>
+#include <gtest/gtest.h>
+#include <nccl.h>
+#include "../include/comm_threaded.h"
+
+TEST(CommThreadedTest, Clique) {
+  CUDACHECKIGNORE(cudaSetDevice(0));
+
+  std::vector<int> gpuClique{0, 1};
+  auto clique = std::make_unique<threadedRanksClique>(gpuClique);
+  clique->insertRank(0, 0);
+
+  EXPECT_EQ(clique->gpus.size(), 2);
+  EXPECT_NE(clique->barrierMbox[0], nullptr);
+  EXPECT_NE(clique->barrierMbox[1], nullptr);
+}
