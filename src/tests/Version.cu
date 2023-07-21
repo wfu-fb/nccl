b4370718f0e6cc757898d49e3173333f8f3735b0
Min Si <msi@meta.com>
2023-07-21 16:56:13 -0700

    Explicitly build tests with nccl2.18.3
    Summary:
    This patch explicitly uses nccl2.18.3 target in 2.18.3 internal unit test. It allows the tests to be triggered in CI with the corresponding nccl-exp version, without -c hpc_comms.use_ncclexp.
    
    This patch also adds a simple unit test Version.cu to check whether the right nccl-exp is used.
    
    Reviewed By: cenzhaometa
    
    Differential Revision: D47581004
    
    fbshipit-source-id: 397fa79173819746c57a56f0f45120e6751249fc

---
+++ src/tests/Version.cu
@@ -0,0 +1,9 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include <iostream>
+#include <gtest/gtest.h>
+#include <nccl.h>
+
+TEST(Version, Code) {
+  EXPECT_EQ(NCCL_VERSION_CODE, 21803);
+}
