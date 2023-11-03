83311abc88f462610c872c71b22d034edc1a35d7
Min Si <msi@meta.com>
2023-11-03 13:13:15 -0700

    guess default DEV_SIGNATURE if under git repo
    Summary: See title. It also fix typo DEV_SIGNITURE->DEV_SIGNATURE
    
    Reviewed By: kingchc
    
    Differential Revision: D50977737
    
    fbshipit-source-id: 27c6883885a7e8e39144e37cf5b951d11af9fd53

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -x
 
+hash=""
+# guess default DEV_SIGNATURE
+is_git=$(git rev-parse --is-inside-work-tree)
+if [ $is_git ]; then
+    hash="git-"$(git rev-parse --short HEAD)
+fi
+
 BUILDDIR=${BUILDDIR:=/tmp/nccl-exp/genai-2.18.3/build}
 NVCC_ARCH=${NVCC_ARCH:="a100,h100"}
 CUDA_HOME=${CUDA_HOME:="/usr/local/cuda"}
 # TODO: automatically get hg id or git hash
-DEV_SIGNITURE=${DEV_SIGNITURE:="none"}
+DEV_SIGNATURE=${DEV_SIGNATURE:="$hash"}
 
 echo "BUILDDIR=${BUILDDIR}"
 echo "NVCC_ARCH=${NVCC_ARCH}"
 echo "CUDA_HOME=${CUDA_HOME}"
-echo "DEV_SIGNITURE=${DEV_SIGNITURE}"
+echo "DEV_SIGNATURE=${DEV_SIGNATURE}"
 
 IFS=',' read -ra arch_array <<< "$NVCC_ARCH"
 arch_gencode=""
@@ -36,4 +43,4 @@ echo "NVCC_GENCODE=$arch_gencode"
 
 # build libnccl
 export BUILDDIR=$BUILDDIR
-make src.clean && make -j src.build NVCC_GENCODE="$arch_gencode" CUDA_HOME="$CUDA_HOME" DEV_SIGNITURE="$DEV_SIGNITURE"
+make src.clean && make -j src.build NVCC_GENCODE="$arch_gencode" CUDA_HOME="$CUDA_HOME" DEV_SIGNATURE="$DEV_SIGNATURE"
