c5206c99b6c8a8fe9423b5c1ebf9d2b1450f6ff8
Wes Bland <wbland@meta.com>
2023-11-16 15:52:12 -0800

    Add HelloWorld test
    Summary: Add an example of an integration test using remote execution.
    
    Reviewed By: kingchc
    
    Differential Revision: D51373133
    
    fbshipit-source-id: 59f1a5e0080a8857f1b322c9f53c3e205b9c114b

--- src/tests/tests_common.cuh
+++ src/tests/tests_common.cuh
@@ -4,6 +4,8 @@
 #define TESTS_COMMON_CUH_
 
 #include "cuda.h"
+#include "mpi.h"
+#include <tuple>
 
 // Typed helper functions
 template <typename T>
@@ -51,5 +53,93 @@ DECL_TYPED_KERNS(uint64_t);
 DECL_TYPED_KERNS(float);
 DECL_TYPED_KERNS(double);
 // Skip half and __nv_bfloat16 since already declared with specific type
+
+#define MPICHECK_TEST(cmd) do {                          \
+  int e = cmd;                                      \
+  if( e != MPI_SUCCESS ) {                          \
+    printf("Failed: MPI error %s:%d '%d'\n",        \
+        __FILE__,__LINE__, e);   \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+#define CUDACHECK_TEST(cmd) do {                         \
+  cudaError_t e = cmd;                              \
+  if( e != cudaSuccess ) {                          \
+    printf("Failed: Cuda error %s:%d '%s'\n",             \
+        __FILE__,__LINE__,cudaGetErrorString(e));   \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+#define NCCLCHECK_TEST(cmd) do {                         \
+  ncclResult_t r = cmd;                             \
+  if (r!= ncclSuccess) {                            \
+    printf("Failed, NCCL error %s:%d '%s'\n",             \
+        __FILE__,__LINE__,ncclGetErrorString(r));   \
+    exit(EXIT_FAILURE);                             \
+  }                                                 \
+} while(0)
+
+static uint64_t getHostHash(const char* string) {
+  // Based on DJB2a, result = result * 33 ^ char
+  uint64_t result = 5381;
+  for (int c = 0; string[c] != '\0'; c++){
+    result = ((result << 5) + result) ^ string[c];
+  }
+  return result;
+}
+
+static void getHostName(char* hostname, int maxlen) {
+  gethostname(hostname, maxlen);
+  for (int i=0; i< maxlen; i++) {
+    if (hostname[i] == '.') {
+        hostname[i] = '\0';
+        return;
+    }
+  }
+}
+
+std::tuple<int, int, int, ncclComm_t> setupNccl(int argc, char **argv) {
+  int localRank, globalRank, numRanks = 0;
+  ncclUniqueId id;
+  ncclComm_t comm;
+
+  //initializing MPI
+  MPICHECK_TEST(MPI_Init(&argc, &argv));
+  MPICHECK_TEST(MPI_Comm_rank(MPI_COMM_WORLD, &globalRank));
+  MPICHECK_TEST(MPI_Comm_size(MPI_COMM_WORLD, &numRanks));
+
+  //calculating localRank based on hostname which is used in selecting a GPU
+  uint64_t hostHashs[numRanks];
+  char hostname[1024];
+  getHostName(hostname, 1024);
+  hostHashs[globalRank] = getHostHash(hostname);
+  MPICHECK_TEST(MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, hostHashs, sizeof(uint64_t), MPI_BYTE, MPI_COMM_WORLD));
+  for (int p=0; p<numRanks; p++) {
+     if (p == globalRank) break;
+     if (hostHashs[p] == hostHashs[globalRank]) localRank++;
+  }
+
+  //get NCCL unique ID at rank 0 and broadcast it to all others
+  if (globalRank == 0) ncclGetUniqueId(&id);
+  MPICHECK_TEST(MPI_Bcast((void *)&id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
+
+  //picking a GPU based on localRank, allocate device buffers
+  CUDACHECK_TEST(cudaSetDevice(localRank));
+
+  //initializing NCCL
+  NCCLCHECK_TEST(ncclCommInitRank(&comm, numRanks, id, globalRank));
+
+  return std::make_tuple(localRank, globalRank, numRanks, comm);
+}
+
+void cleanupNccl(ncclComm_t globalComm) {
+  //finalizing NCCL
+  ncclCommDestroy(globalComm);
+
+  //finalizing MPI
+  MPICHECK_TEST(MPI_Finalize());
+}
 
 #endif
