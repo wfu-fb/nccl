344924a6e2bc5caccf7b7661c1e3155b08ef75a2
Min Si <msi@meta.com>
2023-11-20 22:48:09 -0800

    refactor tests wrapper to support integration test and distributed UT
    Summary:
    We want to reuse the launcher wrapper to run UT in distributed environment and/or with backend network. This patch made two refactors to support it:
    1. Define target of tests_common.cuh so that tests in other directory can find it.
    2. Optionally pass ENVS and HOSTS as env var  to run on remote host
    3. Define `nccl_cpp_distributed_unittest` entry which needs gtest dependency.
    4. Decouple MPI init and ncclComm creation in tests_common.cuh, so that we can reuse in distributed UT
    
    Reviewed By: cenzhaometa
    
    Differential Revision: D51445675
    
    fbshipit-source-id: f5c1ebc0f8786a228ee097e9174bbcbc7b0798ca

--- src/tests/tests_common.cuh
+++ src/tests/tests_common.cuh
@@ -81,65 +81,66 @@ DECL_TYPED_KERNS(double);
   }                                                 \
 } while(0)
 
-static uint64_t getHostHash(const char* string) {
-  // Based on DJB2a, result = result * 33 ^ char
-  uint64_t result = 5381;
-  for (int c = 0; string[c] != '\0'; c++){
-    result = ((result << 5) + result) ^ string[c];
-  }
-  return result;
-}
-
-static void getHostName(char* hostname, int maxlen) {
-  gethostname(hostname, maxlen);
-  for (int i=0; i< maxlen; i++) {
-    if (hostname[i] == '.') {
-        hostname[i] = '\0';
-        return;
-    }
-  }
-}
-
-std::tuple<int, int, int, ncclComm_t> setupNccl(int argc, char **argv) {
+void initializeMpi(int argc, char **argv) {
   int localRank, globalRank, numRanks = 0;
-  ncclUniqueId id;
-  ncclComm_t comm;
 
   //initializing MPI
   MPICHECK_TEST(MPI_Init(&argc, &argv));
+}
+
+std::tuple<int, int, int> getMpiInfo() {
+  int localRank, globalRank, numRanks = 0;
+
   MPICHECK_TEST(MPI_Comm_rank(MPI_COMM_WORLD, &globalRank));
   MPICHECK_TEST(MPI_Comm_size(MPI_COMM_WORLD, &numRanks));
 
-  //calculating localRank based on hostname which is used in selecting a GPU
-  uint64_t hostHashs[numRanks];
-  char hostname[1024];
-  getHostName(hostname, 1024);
-  hostHashs[globalRank] = getHostHash(hostname);
-  MPICHECK_TEST(MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, hostHashs, sizeof(uint64_t), MPI_BYTE, MPI_COMM_WORLD));
-  for (int p=0; p<numRanks; p++) {
-     if (p == globalRank) break;
-     if (hostHashs[p] == hostHashs[globalRank]) localRank++;
-  }
+  MPI_Comm localComm = MPI_COMM_NULL;
+  MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, globalRank, MPI_INFO_NULL, &localComm);
+  MPI_Comm_rank(localComm, &localRank);
+  MPI_Comm_free(&localComm);
+
+  return std::make_tuple(localRank, globalRank, numRanks);
+}
+
+void finalizeMpi() {
+  //finalizing MPI
+  MPICHECK_TEST(MPI_Finalize());
+}
+
+// Convenience function to create a NCCL comm with all global ranks
+// For creating comm with subset of ranks, would need call the internal steps directly.
+ncclComm_t createNcclComm(int globalRank, int numRanks, int devId) {
+  ncclUniqueId id;
+  ncclComm_t comm;
 
   //get NCCL unique ID at rank 0 and broadcast it to all others
   if (globalRank == 0) ncclGetUniqueId(&id);
   MPICHECK_TEST(MPI_Bcast((void *)&id, sizeof(id), MPI_BYTE, 0, MPI_COMM_WORLD));
 
-  //picking a GPU based on localRank, allocate device buffers
-  CUDACHECK_TEST(cudaSetDevice(localRank));
+  CUDACHECK_TEST(cudaSetDevice(devId));
 
   //initializing NCCL
   NCCLCHECK_TEST(ncclCommInitRank(&comm, numRanks, id, globalRank));
+  return comm;
+}
 
-  return std::make_tuple(localRank, globalRank, numRanks, comm);
+std::tuple<int, int, int, ncclComm_t> setupNccl(int argc, char **argv) {
+  int localRank, globalRank, numRanks = 0;
+  ncclComm_t globalComm;
+
+  initializeMpi(argc, argv);
+  std::tie(localRank, globalRank, numRanks) = getMpiInfo();
+
+  globalComm = createNcclComm(globalRank, numRanks, localRank);
+
+  return std::make_tuple(localRank, globalRank, numRanks, globalComm);
 }
 
 void cleanupNccl(ncclComm_t globalComm) {
   //finalizing NCCL
   ncclCommDestroy(globalComm);
 
-  //finalizing MPI
-  MPICHECK_TEST(MPI_Finalize());
+  finalizeMpi();
 }
 
 #endif
