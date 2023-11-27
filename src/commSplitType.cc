9488a76ac183613136dd3119840277fdf023c499
Pavan Balaji <pavanbalaji@meta.com>
2023-11-27 12:26:35 -0800

    Backport ncclCommSplitType to v2.18.3
    Summary: As the title suggests
    
    Reviewed By: cenzhaometa
    
    Differential Revision: D51572997
    
    fbshipit-source-id: 49aa222c1a16a85e01c3d1335c9771ea636d8dde

---
+++ src/commSplitType.cc
@@ -0,0 +1,21 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "nccl.h"
+#include "argcheck.h"
+#include "comm.h"
+
+NCCL_API(ncclResult_t, ncclCommSplitType, ncclComm_t comm, int type, int key, ncclComm_t *newcomm, ncclConfig_t *config);
+ncclResult_t ncclCommSplitType(ncclComm_t comm, int type, int key, ncclComm_t *newcomm, ncclConfig_t *config) {
+  int color;
+
+  if (type == NCCL_SPLIT_TYPE_UNDEFINED) {
+    color = NCCL_SPLIT_NOCOLOR;
+  } else if (type == NCCL_SPLIT_TYPE_NODE) {
+    color = comm->rankToNode[comm->rank];
+  } else {
+    if (newcomm) *newcomm = NULL;
+    return ncclInvalidArgument;
+  }
+
+  return ncclCommSplit(comm, color, key, newcomm, config);
+}
