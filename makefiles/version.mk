c0c2f85ae0929c551fb280579667e9343ba9a903
Wes Bland <wbland@meta.com>
2024-01-02 13:31:17 -0800

    Remove genai branch
    Summary: This branch is no longer needed now that all of the contents have moved back to the 2.18.3 branch.
    
    Reviewed By: kingchc
    
    Differential Revision: D52216834
    
    fbshipit-source-id: a073d86b60bd8571543d874a6d296898a409a66a

--- makefiles/version.mk
+++
@@ -1,6 +0,0 @@
-##### version
-NCCL_MAJOR   := 2
-NCCL_MINOR   := 18
-NCCL_PATCH   := 3
-NCCL_SUFFIX  := meta-exp
-PKG_REVISION := 1
--- src/tests/Version.cu
+++
@@ -1,9 +0,0 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-#include <iostream>
-#include <gtest/gtest.h>
-#include <nccl.h>
-
-TEST(Version, Code) {
-  EXPECT_EQ(NCCL_VERSION_CODE, 21803);
-}
